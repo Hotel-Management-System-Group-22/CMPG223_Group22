7ff8fdac77d1aad9f5847a5e31dcd908aebba3ec
GoitseoneMoalusi <goitseonemoalusi1@gmail.com>
2024-08-17 05:35:39 +0200

    Change in Layout
    Change in Layout

--- HotelManagementSystem/Guests.Designer.cs
+++ HotelManagementSystem/Guests.Designer.cs
@@ -28,195 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvGuests = new System.Windows.Forms.DataGridView();
-            this.txtGuest_Email = new System.Windows.Forms.TextBox();
-            this.txtGuest_ContactNo = new System.Windows.Forms.TextBox();
-            this.txtGuest_LName = new System.Windows.Forms.TextBox();
-            this.txtGuestFName = new System.Windows.Forms.TextBox();
-            this.txtGuestID = new System.Windows.Forms.TextBox();
-            this.lblEmail = new System.Windows.Forms.Label();
-            this.LblContactNo = new System.Windows.Forms.Label();
-            this.LblLName = new System.Windows.Forms.Label();
-            this.LblFName = new System.Windows.Forms.Label();
-            this.LblGuest = new System.Windows.Forms.Label();
-            this.BtnAddGuest = new System.Windows.Forms.Button();
-            this.BtnEditGuest = new System.Windows.Forms.Button();
-            this.BtnDelete = new System.Windows.Forms.Button();
-            this.BtnCancelGuest = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvGuests)).BeginInit();
+            this.grpBoxMenu = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbPgAddGuest = new System.Windows.Forms.TabPage();
+            this.tbPageSearchGuest = new System.Windows.Forms.TabPage();
+            this.tbPgUpdateGuest = new System.Windows.Forms.TabPage();
+            this.tbPageDeleteGuest = new System.Windows.Forms.TabPage();
+            this.DgvGuests = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.grpBoxMenu.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tbPgAddGuest.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvGuests)).BeginInit();
             this.SuspendLayout();
             // 
-            // dgvGuests
+            // grpBoxMenu
             // 
-            this.dgvGuests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvGuests.Location = new System.Drawing.Point(12, 23);
-            this.dgvGuests.Name = "dgvGuests";
-            this.dgvGuests.RowHeadersWidth = 51;
-            this.dgvGuests.RowTemplate.Height = 24;
-            this.dgvGuests.Size = new System.Drawing.Size(405, 348);
-            this.dgvGuests.TabIndex = 0;
+            this.grpBoxMenu.Controls.Add(this.button4);
+            this.grpBoxMenu.Controls.Add(this.button3);
+            this.grpBoxMenu.Controls.Add(this.button2);
+            this.grpBoxMenu.Controls.Add(this.button1);
+            this.grpBoxMenu.Location = new System.Drawing.Point(937, 12);
+            this.grpBoxMenu.Name = "grpBoxMenu";
+            this.grpBoxMenu.Size = new System.Drawing.Size(268, 297);
+            this.grpBoxMenu.TabIndex = 0;
+            this.grpBoxMenu.TabStop = false;
+            this.grpBoxMenu.Text = "Menu";
             // 
-            // txtGuest_Email
+            // button1
             // 
-            this.txtGuest_Email.Location = new System.Drawing.Point(633, 368);
-            this.txtGuest_Email.Name = "txtGuest_Email";
-            this.txtGuest_Email.Size = new System.Drawing.Size(100, 22);
-            this.txtGuest_Email.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(11, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD GUEST";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // txtGuest_ContactNo
+            // button2
             // 
-            this.txtGuest_ContactNo.Location = new System.Drawing.Point(633, 283);
-            this.txtGuest_ContactNo.Name = "txtGuest_ContactNo";
-            this.txtGuest_ContactNo.Size = new System.Drawing.Size(100, 22);
-            this.txtGuest_ContactNo.TabIndex = 2;
+            this.button2.Location = new System.Drawing.Point(11, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SEARCH GUEST ";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // txtGuest_LName
+            // button3
             // 
-            this.txtGuest_LName.Location = new System.Drawing.Point(633, 213);
-            this.txtGuest_LName.Name = "txtGuest_LName";
-            this.txtGuest_LName.Size = new System.Drawing.Size(100, 22);
-            this.txtGuest_LName.TabIndex = 3;
+            this.button3.Location = new System.Drawing.Point(11, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(243, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "UPDATE GUEST";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // txtGuestFName
+            // button4
             // 
-            this.txtGuestFName.Location = new System.Drawing.Point(633, 122);
-            this.txtGuestFName.Name = "txtGuestFName";
-            this.txtGuestFName.Size = new System.Drawing.Size(100, 22);
-            this.txtGuestFName.TabIndex = 4;
+            this.button4.Location = new System.Drawing.Point(11, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(243, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "DELETE GUEST";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // txtGuestID
+            // tabControl1
             // 
-            this.txtGuestID.Location = new System.Drawing.Point(633, 47);
-            this.txtGuestID.Name = "txtGuestID";
-            this.txtGuestID.Size = new System.Drawing.Size(100, 22);
-            this.txtGuestID.TabIndex = 5;
+            this.tabControl1.Controls.Add(this.tbPgAddGuest);
+            this.tabControl1.Controls.Add(this.tbPageSearchGuest);
+            this.tabControl1.Controls.Add(this.tbPgUpdateGuest);
+            this.tabControl1.Controls.Add(this.tbPageDeleteGuest);
+            this.tabControl1.Location = new System.Drawing.Point(38, 30);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(762, 279);
+            this.tabControl1.TabIndex = 1;
             // 
-            // lblEmail
+            // tbPgAddGuest
             // 
-            this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(510, 368);
-            this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(44, 16);
-            this.lblEmail.TabIndex = 6;
-            this.lblEmail.Text = "Email:";
+            this.tbPgAddGuest.Controls.Add(this.label1);
+            this.tbPgAddGuest.Controls.Add(this.label2);
+            this.tbPgAddGuest.Controls.Add(this.label3);
+            this.tbPgAddGuest.Controls.Add(this.label4);
+            this.tbPgAddGuest.Location = new System.Drawing.Point(4, 25);
+            this.tbPgAddGuest.Name = "tbPgAddGuest";
+            this.tbPgAddGuest.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgAddGuest.Size = new System.Drawing.Size(754, 250);
+            this.tbPgAddGuest.TabIndex = 0;
+            this.tbPgAddGuest.Text = "ADD GUEST";
+            this.tbPgAddGuest.UseVisualStyleBackColor = true;
             // 
-            // LblContactNo
+            // tbPageSearchGuest
             // 
-            this.LblContactNo.AutoSize = true;
-            this.LblContactNo.Location = new System.Drawing.Point(510, 289);
-            this.LblContactNo.Name = "LblContactNo";
-            this.LblContactNo.Size = new System.Drawing.Size(106, 16);
-            this.LblContactNo.TabIndex = 7;
-            this.LblContactNo.Text = "Contact Number:";
+            this.tbPageSearchGuest.Location = new System.Drawing.Point(4, 25);
+            this.tbPageSearchGuest.Name = "tbPageSearchGuest";
+            this.tbPageSearchGuest.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPageSearchGuest.Size = new System.Drawing.Size(754, 219);
+            this.tbPageSearchGuest.TabIndex = 1;
+            this.tbPageSearchGuest.Text = "SEARCH GUEST";
+            this.tbPageSearchGuest.UseVisualStyleBackColor = true;
             // 
-            // LblLName
+            // tbPgUpdateGuest
             // 
-            this.LblLName.AutoSize = true;
-            this.LblLName.Location = new System.Drawing.Point(510, 219);
-            this.LblLName.Name = "LblLName";
-            this.LblLName.Size = new System.Drawing.Size(75, 16);
-            this.LblLName.TabIndex = 8;
-            this.LblLName.Text = "Last Name:";
+            this.tbPgUpdateGuest.Location = new System.Drawing.Point(4, 25);
+            this.tbPgUpdateGuest.Name = "tbPgUpdateGuest";
+            this.tbPgUpdateGuest.Size = new System.Drawing.Size(754, 219);
+            this.tbPgUpdateGuest.TabIndex = 2;
+            this.tbPgUpdateGuest.Text = "UPDATE GUEST";
+            this.tbPgUpdateGuest.UseVisualStyleBackColor = true;
             // 
-            // LblFName
+            // tbPageDeleteGuest
             // 
-            this.LblFName.AutoSize = true;
-            this.LblFName.Location = new System.Drawing.Point(510, 128);
-            this.LblFName.Name = "LblFName";
-            this.LblFName.Size = new System.Drawing.Size(75, 16);
-            this.LblFName.TabIndex = 9;
-            this.LblFName.Text = "First Name:";
+            this.tbPageDeleteGuest.Location = new System.Drawing.Point(4, 25);
+            this.tbPageDeleteGuest.Name = "tbPageDeleteGuest";
+            this.tbPageDeleteGuest.Size = new System.Drawing.Size(754, 219);
+            this.tbPageDeleteGuest.TabIndex = 3;
+            this.tbPageDeleteGuest.Text = "DELETE GUEST";
+            this.tbPageDeleteGuest.UseVisualStyleBackColor = true;
             // 
-            // LblGuest
+            // DgvGuests
             // 
-            this.LblGuest.AutoSize = true;
-            this.LblGuest.Location = new System.Drawing.Point(453, 53);
-            this.LblGuest.Name = "LblGuest";
-            this.LblGuest.Size = new System.Drawing.Size(132, 16);
-            this.LblGuest.TabIndex = 10;
-            this.LblGuest.Text = "Identification Number";
+            this.DgvGuests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvGuests.Location = new System.Drawing.Point(38, 343);
+            this.DgvGuests.Name = "DgvGuests";
+            this.DgvGuests.RowHeadersWidth = 51;
+            this.DgvGuests.RowTemplate.Height = 24;
+            this.DgvGuests.Size = new System.Drawing.Size(1167, 215);
+            this.DgvGuests.TabIndex = 2;
             // 
-            // BtnAddGuest
+            // label1
             // 
-            this.BtnAddGuest.Location = new System.Drawing.Point(12, 402);
-            this.BtnAddGuest.Name = "BtnAddGuest";
-            this.BtnAddGuest.Size = new System.Drawing.Size(75, 23);
-            this.BtnAddGuest.TabIndex = 11;
-            this.BtnAddGuest.Text = "Add Guest";
-            this.BtnAddGuest.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
-            // BtnEditGuest
+            // label2
             // 
-            this.BtnEditGuest.Location = new System.Drawing.Point(116, 402);
-            this.BtnEditGuest.Name = "BtnEditGuest";
-            this.BtnEditGuest.Size = new System.Drawing.Size(75, 23);
-            this.BtnEditGuest.TabIndex = 12;
-            this.BtnEditGuest.Text = "Edit Guest";
-            this.BtnEditGuest.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
             // 
-            // BtnDelete
+            // label3
             // 
-            this.BtnDelete.Location = new System.Drawing.Point(251, 402);
-            this.BtnDelete.Name = "BtnDelete";
-            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
-            this.BtnDelete.TabIndex = 13;
-            this.BtnDelete.Text = "Delete Guest";
-            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
             // 
-            // BtnCancelGuest
+            // label4
             // 
-            this.BtnCancelGuest.Location = new System.Drawing.Point(394, 402);
-            this.BtnCancelGuest.Name = "BtnCancelGuest";
-            this.BtnCancelGuest.Size = new System.Drawing.Size(75, 23);
-            this.BtnCancelGuest.TabIndex = 14;
-            this.BtnCancelGuest.Text = "Cancel";
-            this.BtnCancelGuest.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
             // 
             // Guests
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.BtnCancelGuest);
-            this.Controls.Add(this.BtnDelete);
-            this.Controls.Add(this.BtnEditGuest);
-            this.Controls.Add(this.BtnAddGuest);
-            this.Controls.Add(this.LblGuest);
-            this.Controls.Add(this.LblFName);
-            this.Controls.Add(this.LblLName);
-            this.Controls.Add(this.LblContactNo);
-            this.Controls.Add(this.lblEmail);
-            this.Controls.Add(this.txtGuestID);
-            this.Controls.Add(this.txtGuestFName);
-            this.Controls.Add(this.txtGuest_LName);
-            this.Controls.Add(this.txtGuest_ContactNo);
-            this.Controls.Add(this.txtGuest_Email);
-            this.Controls.Add(this.dgvGuests);
+            this.ClientSize = new System.Drawing.Size(1259, 592);
+            this.Controls.Add(this.DgvGuests);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.grpBoxMenu);
             this.Name = "Guests";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvGuests)).EndInit();
+            this.Text = "Guests";
+            this.grpBoxMenu.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tbPgAddGuest.ResumeLayout(false);
+            this.tbPgAddGuest.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvGuests)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvGuests;
-        private System.Windows.Forms.TextBox txtGuest_Email;
-        private System.Windows.Forms.TextBox txtGuest_ContactNo;
-        private System.Windows.Forms.TextBox txtGuest_LName;
-        private System.Windows.Forms.TextBox txtGuestFName;
-        private System.Windows.Forms.TextBox txtGuestID;
-        private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.Label LblContactNo;
-        private System.Windows.Forms.Label LblLName;
-        private System.Windows.Forms.Label LblFName;
-        private System.Windows.Forms.Label LblGuest;
-        private System.Windows.Forms.Button BtnAddGuest;
-        private System.Windows.Forms.Button BtnEditGuest;
-        private System.Windows.Forms.Button BtnDelete;
-        private System.Windows.Forms.Button BtnCancelGuest;
+        private System.Windows.Forms.GroupBox grpBoxMenu;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbPgAddGuest;
+        private System.Windows.Forms.TabPage tbPageSearchGuest;
+        private System.Windows.Forms.TabPage tbPgUpdateGuest;
+        private System.Windows.Forms.TabPage tbPageDeleteGuest;
+        private System.Windows.Forms.DataGridView DgvGuests;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
