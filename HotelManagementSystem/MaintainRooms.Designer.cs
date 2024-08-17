544643810607764d18d0fc1d0e617d4ecc493431
StephenNel <stephennel321@gmail.com>
2024-08-18 00:39:52 +0200

    search delete and some other form stuff

--- HotelManagementSystem/MaintainRooms.Designer.cs
+++ HotelManagementSystem/MaintainRooms.Designer.cs
@@ -42,17 +42,12 @@
             this.lblRoomID = new System.Windows.Forms.Label();
             this.btnAddRoom = new System.Windows.Forms.Button();
             this.btnAddReset = new System.Windows.Forms.Button();
-            this.nrudRoomRate = new System.Windows.Forms.NumericUpDown();
-            this.cbxOccupancy = new System.Windows.Forms.ComboBox();
             this.cbxRoomStatus = new System.Windows.Forms.ComboBox();
-            this.cbxRoomType = new System.Windows.Forms.ComboBox();
-            this.txtRoomNumber = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tbpDelete = new System.Windows.Forms.TabPage();
+            this.btnDeleteReset = new System.Windows.Forms.Button();
             this.btnDeleteRoom = new System.Windows.Forms.Button();
             this.txtDeleteRoomID = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -63,11 +58,9 @@
             this.btnMenuUpdate = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnDeleteReset = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tbpSearch.SuspendLayout();
             this.tbpAdd_Update.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nrudRoomRate)).BeginInit();
             this.tbpDelete.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -129,18 +122,18 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(9, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Sort by Room number";
+            this.label2.Text = "Sort by Room ID";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(9, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Search Room Number";
+            this.label1.Text = "Search Room ID";
             // 
             // btnSearchReset
             // 
@@ -158,6 +151,7 @@
             this.txtSearchRoom.Name = "txtSearchRoom";
             this.txtSearchRoom.Size = new System.Drawing.Size(100, 20);
             this.txtSearchRoom.TabIndex = 0;
+            this.txtSearchRoom.TextChanged += new System.EventHandler(this.txtSearchRoom_TextChanged);
             // 
             // tbpAdd_Update
             // 
@@ -166,15 +160,9 @@
             this.tbpAdd_Update.Controls.Add(this.lblRoomID);
             this.tbpAdd_Update.Controls.Add(this.btnAddRoom);
             this.tbpAdd_Update.Controls.Add(this.btnAddReset);
-            this.tbpAdd_Update.Controls.Add(this.nrudRoomRate);
-            this.tbpAdd_Update.Controls.Add(this.cbxOccupancy);
             this.tbpAdd_Update.Controls.Add(this.cbxRoomStatus);
-            this.tbpAdd_Update.Controls.Add(this.cbxRoomType);
-            this.tbpAdd_Update.Controls.Add(this.txtRoomNumber);
-            this.tbpAdd_Update.Controls.Add(this.label7);
+            this.tbpAdd_Update.Controls.Add(this.txtEmployeeID);
             this.tbpAdd_Update.Controls.Add(this.label6);
-            this.tbpAdd_Update.Controls.Add(this.label5);
-            this.tbpAdd_Update.Controls.Add(this.label4);
             this.tbpAdd_Update.Controls.Add(this.label3);
             this.tbpAdd_Update.Location = new System.Drawing.Point(4, 22);
             this.tbpAdd_Update.Name = "tbpAdd_Update";
@@ -192,6 +180,7 @@
             this.btnUpdateRoom.TabIndex = 29;
             this.btnUpdateRoom.Text = "Update room";
             this.btnUpdateRoom.UseVisualStyleBackColor = true;
+            this.btnUpdateRoom.Click += new System.EventHandler(this.btnUpdateRoom_Click);
             // 
             // txtRoomID
             // 
@@ -204,7 +193,7 @@
             // lblRoomID
             // 
             this.lblRoomID.AutoSize = true;
-            this.lblRoomID.Location = new System.Drawing.Point(26, 25);
+            this.lblRoomID.Location = new System.Drawing.Point(27, 25);
             this.lblRoomID.Name = "lblRoomID";
             this.lblRoomID.Size = new System.Drawing.Size(49, 13);
             this.lblRoomID.TabIndex = 27;
@@ -219,6 +208,7 @@
             this.btnAddRoom.TabIndex = 12;
             this.btnAddRoom.Text = "Add room";
             this.btnAddRoom.UseVisualStyleBackColor = true;
+            this.btnAddRoom.Click += new System.EventHandler(this.btnAddRoom_Click);
             // 
             // btnAddReset
             // 
@@ -230,88 +220,43 @@
             this.btnAddReset.UseVisualStyleBackColor = true;
             this.btnAddReset.Click += new System.EventHandler(this.btnAddReset_Click);
             // 
-            // nrudRoomRate
-            // 
-            this.nrudRoomRate.Location = new System.Drawing.Point(157, 107);
-            this.nrudRoomRate.Name = "nrudRoomRate";
-            this.nrudRoomRate.Size = new System.Drawing.Size(120, 20);
-            this.nrudRoomRate.TabIndex = 10;
-            // 
-            // cbxOccupancy
-            // 
-            this.cbxOccupancy.FormattingEnabled = true;
-            this.cbxOccupancy.Location = new System.Drawing.Point(156, 163);
-            this.cbxOccupancy.Name = "cbxOccupancy";
-            this.cbxOccupancy.Size = new System.Drawing.Size(121, 21);
-            this.cbxOccupancy.TabIndex = 9;
-            // 
             // cbxRoomStatus
             // 
             this.cbxRoomStatus.FormattingEnabled = true;
-            this.cbxRoomStatus.Location = new System.Drawing.Point(156, 133);
+            this.cbxRoomStatus.Items.AddRange(new object[] {
+            "Unoccupied",
+            "Occupied"});
+            this.cbxRoomStatus.Location = new System.Drawing.Point(156, 77);
             this.cbxRoomStatus.Name = "cbxRoomStatus";
             this.cbxRoomStatus.Size = new System.Drawing.Size(121, 21);
             this.cbxRoomStatus.TabIndex = 8;
+            this.cbxRoomStatus.SelectedIndexChanged += new System.EventHandler(this.cbxRoomStatus_SelectedIndexChanged);
             // 
-            // cbxRoomType
+            // txtEmployeeID
             // 
-            this.cbxRoomType.FormattingEnabled = true;
-            this.cbxRoomType.Location = new System.Drawing.Point(156, 74);
-            this.cbxRoomType.Name = "cbxRoomType";
-            this.cbxRoomType.Size = new System.Drawing.Size(121, 21);
-            this.cbxRoomType.TabIndex = 6;
-            // 
-            // txtRoomNumber
-            // 
-            this.txtRoomNumber.Location = new System.Drawing.Point(156, 44);
-            this.txtRoomNumber.Name = "txtRoomNumber";
-            this.txtRoomNumber.Size = new System.Drawing.Size(121, 20);
-            this.txtRoomNumber.TabIndex = 5;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(26, 163);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(83, 13);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Max occupancy";
+            this.txtEmployeeID.Location = new System.Drawing.Point(156, 44);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(121, 20);
+            this.txtEmployeeID.TabIndex = 5;
+            this.txtEmployeeID.TextChanged += new System.EventHandler(this.txtEmployeeID_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(26, 133);
+            this.label6.Location = new System.Drawing.Point(26, 77);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(66, 13);
             this.label6.TabIndex = 3;
             this.label6.Text = "Room status";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 104);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Room rate";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(26, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Room type";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(26, 51);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 0;
-            this.label3.Text = "Room number";
+            this.label3.Text = "Employee ID";
             // 
             // tbpDelete
             // 
@@ -326,6 +271,16 @@
             this.tbpDelete.Text = "Delete";
             this.tbpDelete.UseVisualStyleBackColor = true;
             // 
+            // btnDeleteReset
+            // 
+            this.btnDeleteReset.Location = new System.Drawing.Point(333, 45);
+            this.btnDeleteReset.Name = "btnDeleteReset";
+            this.btnDeleteReset.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteReset.TabIndex = 3;
+            this.btnDeleteReset.Text = "Reset";
+            this.btnDeleteReset.UseVisualStyleBackColor = true;
+            this.btnDeleteReset.Click += new System.EventHandler(this.btnDeleteReset_Click);
+            // 
             // btnDeleteRoom
             // 
             this.btnDeleteRoom.Location = new System.Drawing.Point(131, 111);
@@ -334,6 +289,7 @@
             this.btnDeleteRoom.TabIndex = 2;
             this.btnDeleteRoom.Text = "Delete room";
             this.btnDeleteRoom.UseVisualStyleBackColor = true;
+            this.btnDeleteRoom.Click += new System.EventHandler(this.btnDeleteRoom_Click);
             // 
             // txtDeleteRoomID
             // 
@@ -341,6 +297,7 @@
             this.txtDeleteRoomID.Name = "txtDeleteRoomID";
             this.txtDeleteRoomID.Size = new System.Drawing.Size(100, 20);
             this.txtDeleteRoomID.TabIndex = 1;
+            this.txtDeleteRoomID.TextChanged += new System.EventHandler(this.txtDeleteRoomID_TextChanged);
             // 
             // label8
             // 
@@ -411,8 +368,10 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 269);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(733, 273);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // panel1
             // 
@@ -420,16 +379,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1014, 15);
             this.panel1.TabIndex = 4;
-            // 
-            // btnDeleteReset
-            // 
-            this.btnDeleteReset.Location = new System.Drawing.Point(333, 45);
-            this.btnDeleteReset.Name = "btnDeleteReset";
-            this.btnDeleteReset.Size = new System.Drawing.Size(75, 23);
-            this.btnDeleteReset.TabIndex = 3;
-            this.btnDeleteReset.Text = "Reset";
-            this.btnDeleteReset.UseVisualStyleBackColor = true;
-            this.btnDeleteReset.Click += new System.EventHandler(this.btnDeleteReset_Click);
             // 
             // MaintainRooms
             // 
@@ -443,12 +392,12 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MaintainRooms";
             this.Text = "MaintainRooms";
+            this.Load += new System.EventHandler(this.MaintainRooms_Load);
             this.tabControl1.ResumeLayout(false);
             this.tbpSearch.ResumeLayout(false);
             this.tbpSearch.PerformLayout();
             this.tbpAdd_Update.ResumeLayout(false);
             this.tbpAdd_Update.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nrudRoomRate)).EndInit();
             this.tbpDelete.ResumeLayout(false);
             this.tbpDelete.PerformLayout();
             this.groupBox1.ResumeLayout(false);
@@ -476,17 +425,11 @@
         private System.Windows.Forms.Button btnSearchReset;
         private System.Windows.Forms.TextBox txtSearchRoom;
         private System.Windows.Forms.RadioButton rdbDescending;
-        private System.Windows.Forms.ComboBox cbxRoomType;
-        private System.Windows.Forms.TextBox txtRoomNumber;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmployeeID;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnAddRoom;
         private System.Windows.Forms.Button btnAddReset;
-        private System.Windows.Forms.NumericUpDown nrudRoomRate;
-        private System.Windows.Forms.ComboBox cbxOccupancy;
         private System.Windows.Forms.ComboBox cbxRoomStatus;
         private System.Windows.Forms.TextBox txtRoomID;
         private System.Windows.Forms.Label lblRoomID;
--- HotelManagementSystem/MaintainRooms.cs
+++ HotelManagementSystem/MaintainRooms.cs
@@ -5,9 +5,12 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace HotelManagementSystem
 {
@@ -16,14 +19,26 @@ namespace HotelManagementSystem
         public MaintainRooms()
         {
             InitializeComponent();
+
+            btnAddReset.Click += new EventHandler(btnAddReset_Click);
+            btnDeleteReset.Click += new EventHandler(btnDeleteReset_Click);
+            btnSearchReset.Click += new EventHandler(btnDeleteReset_Click);
         }
 
-        string connection = "";
+        string connection = "Data Source=(Localdb)\\MSSQLLocalDB;Database=Cmpg223;Trusted_Connection=True;";
+        int selectedId;
+
 
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
             tabControl1.SelectedIndex = 0;
+            btnSearchReset_Click(this, EventArgs.Empty);
+            btnSearchReset.PerformClick();
+            btnAddReset_Click(this, EventArgs.Empty);
+            btnAddReset.PerformClick();
+            btnDeleteReset_Click(this, EventArgs.Empty);
+            btnDeleteReset.PerformClick();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -33,6 +48,7 @@ namespace HotelManagementSystem
             btnAddRoom.Visible = true;
             txtRoomID.Visible = false;
             lblRoomID.Visible = false;
+            LoadData();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
@@ -42,6 +58,7 @@ namespace HotelManagementSystem
             lblRoomID.Visible = true;
             btnUpdateRoom.Visible = true;
             btnAddRoom.Visible = false;
+            LoadData();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
@@ -69,7 +86,7 @@ namespace HotelManagementSystem
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-
+            UpdateEmployeeDataGridView(txtRoomID.Text, dataGridView1, "SELECT * FROM Room WHERE Room_ID LIKE @searchTerm");
         }
 
         private void rdbDescending_CheckedChanged(object sender, EventArgs e)
@@ -77,8 +94,21 @@ namespace HotelManagementSystem
             if (rdbDescending.Checked)
             {
                 rdbAscending.Checked = false;
+                string query = "SELECT * FROM Room ORDER BY Room_ID DESC"; // Sort by Room_ID in descending order
+
+                // Step 3: Connect to the database and fill the DataGridView
+                using (SqlConnection conn = new SqlConnection(connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Step 4: Bind the data to the DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+
             }
-            
+
         }
 
         private void rdbAscending_CheckedChanged(object sender, EventArgs e)
@@ -87,27 +117,211 @@ namespace HotelManagementSystem
             if (rdbAscending.Checked)
             {
                 rdbDescending.Checked = false;
+
+                string query = "SELECT * FROM Room ORDER BY Room_ID ASC"; // Change to DESC for descending order
+
+                // Step 3: Connect to the database and fill the DataGridView
+                using (SqlConnection conn = new SqlConnection(connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Step 4: Bind the data to the DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
             }
         }
 
         private void btnSearchReset_Click(object sender, EventArgs e)
         {
             txtSearchRoom.Text = string.Empty;
+            rdbAscending.Checked = true;
         }
 
         private void btnAddReset_Click(object sender, EventArgs e)
         {
             txtRoomID.Text = string.Empty;  
-            txtRoomNumber.Text = string.Empty;  
-            nrudRoomRate.Value = 0;
-            //cbxOccupancy.SelectedIndex = 0;
-           // cbxRoomStatus.SelectedIndex = 0;
-            //cbxRoomType.SelectedIndex = 0;
+            txtEmployeeID.Text = string.Empty;  
+            cbxRoomStatus.SelectedIndex = -1;
+           
         }
 
         private void btnDeleteReset_Click(object sender, EventArgs e)
         {
             txtDeleteRoomID.Text = string.Empty;    
+        }
+
+        private void MaintainRooms_Load(object sender, EventArgs e)
+        {
+
+            LoadData();
+
+        }
+
+        private void cbxRoomStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateEmployeeDataGridView((cbxRoomStatus.SelectedIndex).ToString(), dataGridView1, "SELECT * FROM Room WHERE Room_Status LIKE @searchTerm");
+        }
+
+        private void UpdateEmployeeDataGridView(string searchTerm, DataGridView dataGridView, string query)
+        {
+
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@searchTerm", $"%{searchTerm.Trim()}%");
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            dataGridView.DataSource = dataTable;
+        }
+
+
+        private void txtSearchRoom_TextChanged(object sender, EventArgs e)
+        {
+            UpdateEmployeeDataGridView(txtSearchRoom.Text, dataGridView1, "SELECT * FROM Room WHERE Room_ID LIKE @searchTerm");
+
+        }
+
+        private void btnDeleteRoom_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                selectedId = Convert.ToInt32(selectedRow.Cells["Room_ID"].Value);
+                DialogResult result = MessageBox.Show("Are you sure you want to Delete this record? \nID: " + selectedId, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    string query = "DELETE FROM Room WHERE Room_ID = " + selectedId;
+
+                    // Use a SqlConnection to connect to the database
+                    using (SqlConnection conn = new SqlConnection(connection))
+                    {
+                        try
+                        {
+                            conn.Open();
+
+                            // Use a SqlCommand to execute the delete query
+                            using (SqlCommand command = new SqlCommand(query, conn))
+                            {
+                                int rowsAffected = command.ExecuteNonQuery();
+                                MessageBox.Show(rowsAffected > 0
+                                    ? "Record deleted successfully."
+                                    : "No record found with the specified ID.");
+                            }
+                            LoadData();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("An error occurred: " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Action Canceled");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No record selected. Please select which job you would like to Delete");
+            }
+
+        }
+
+        private void txtDeleteRoomID_TextChanged(object sender, EventArgs e)
+        {
+            UpdateEmployeeDataGridView(txtDeleteRoomID.Text, dataGridView1, "SELECT * FROM Room WHERE Room_ID LIKE @searchTerm");
+        }
+
+        private void txtEmployeeID_TextChanged(object sender, EventArgs e)
+        {
+            UpdateEmployeeDataGridView(txtEmployeeID.Text, dataGridView1, "SELECT * FROM Room WHERE Employee_ID LIKE @searchTerm");
+        }
+
+        private void btnUpdateRoom_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Room SET Employee_ID = @Value1, Room_Status = @Value2 WHERE Room_ID = @ID";
+            string sRoomStatus = cbxRoomStatus.SelectedIndex.ToString();
+            string sEmployeeID = txtEmployeeID.Text;
+
+            if (sRoomStatus != "-1" && sEmployeeID != "")
+            {
+
+                DialogResult result = MessageBox.Show("Are you sure you want to edit this record? \nID: " + selectedId, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+
+                    using (SqlConnection conn = new SqlConnection(connection))
+                    {
+                        SqlCommand command = new SqlCommand(query, conn);
+
+                        // Add parameters to the SqlCommand
+                        command.Parameters.AddWithValue("@Value1", sEmployeeID);
+                        command.Parameters.AddWithValue("@Value2", sRoomStatus);
+                        command.Parameters.AddWithValue("@ID", selectedId);
+
+                        try
+                        {
+                            conn.Open();
+                            int rowsAffected = command.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                Console.WriteLine("Insert successful.");
+                                LoadData();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Insert failed.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("An error occurred: " + ex.Message);
+                        }
+                    }
+
+                }
+          
+            }
+            else
+            {
+                MessageBox.Show("Fields cannot be Empty");
+            }
+            LoadData();
+
+        }
+
+        private void btnAddRoom_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_SelectionChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if there are any selected rows
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Get the currently selected row
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                selectedId = Convert.ToInt32(selectedRow.Cells["Room_ID"].Value);
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
